986e2d87b56c503c2cbb62eaa10f22b49385ba3d
DuoYunZ <hhbjn@qq.com>
2025-03-07 16:12:07 +0800

    敌人 逻辑

---
+++ Assets/Kawaii Survivor/Scripts/Enemy.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(EnemyMovement))]
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Components")]
+    private EnemyMovement movement;
+
+    [Header("Elements")]
+    private Player player;
+
+    [Header("Spawn Sequence Related ")]
+    [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private SpriteRenderer spawnIndicator;
+    private bool hasSpawned;
+
+
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem passAwayParticles;
+
+    [Header("Attack")]
+    [SerializeField] private int damage;
+    [SerializeField] private float attackFreqwency;
+    [SerializeField] private float playerDetectionRadiusd;
+    private float attackDelay;
+    private float attckTimer;
+
+    [Header("DEBUG")]
+    [SerializeField] private bool gizmos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<EnemyMovement>();
+
+        player = FindFirstObjectByType<Player>();
+        if (player == null)
+        {
+            Debug.LogWarning("No player found,Auto-destroying...");
+            Destroy(gameObject);        
+        }
+        StartSpawnSequence();
+
+        attackDelay = 1f / attackFreqwency;
+        Debug.Log("Attack Delay :" + attackDelay);
+    }
+
+    private void StartSpawnSequence()
+    {
+        SetRenderersVisibility(false);
+
+
+        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
+        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f)
+            .setLoopPingPong(4)
+            .setOnComplete(SpawnSequenceCompleted);
+             
+    }
+    private void SpawnSequenceCompleted()
+    {
+        SetRenderersVisibility();
+        hasSpawned = true;
+
+        movement.StorePlayer(player);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attckTimer >= attackDelay)
+            TrayAttack();
+        else
+            Wait();
+    }
+
+    private void SetRenderersVisibility(bool visibility = true)
+    {
+        renderer.enabled = visibility;
+        spawnIndicator.enabled = !visibility;
+    }
+
+    private void Wait()
+    {
+        attckTimer += Time.deltaTime;
+    }
+
+    private void TrayAttack()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= playerDetectionRadiusd)
+            Attack();
+
+
+    }
+    private void Attack()
+    {
+                attckTimer = 0;
+    }
+    private void PassAway()
+    {
+        // Unparent the particles & play them
+        passAwayParticles.transform.SetParent(null);
+        passAwayParticles.Play();
+
+        Destroy(gameObject);
+    }
+    private void OnDrawGizmos()
+    {
+        if (!gizmos)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRadiusd);
+    }
+}
--- Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -8,62 +8,22 @@ public class EnemyMovement : MonoBehaviour
     [Header("Elements")]
     private Player player;
 
-    [Header("Spawn Sequence Related ")]
-    [SerializeField] private SpriteRenderer renderer;
-    [SerializeField] private SpriteRenderer spawnIndicator;
-    private bool hasSpawned;
+
 
     [Header("Setting")]
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float playerDetectionRadiusd;
-
-    [Header("Elements")]
-    [SerializeField] private ParticleSystem passAwayParticles;
-
-    [Header("DEBUG")]
-    [SerializeField] private bool gizmos;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        player = FindFirstObjectByType<Player>();
-        if(player == null)
-        {
-            Debug.LogWarning("No player found,Auto-destroying...");
-            Destroy(gameObject);
-        }
-
-
-        renderer.enabled = false;
-        spawnIndicator.enabled = true;
-        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
-        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f)
-            .setLoopPingPong(4)
-            .setOnComplete(SpawnSequenceCompleted);
-
-    }
-
+ 
     // Update is called once per frame
     void Update()
     {
-
-        if (!hasSpawned)
-            return;
-
-       
+        if(player !=null)
             FollowPlayer();
-            TrayAttack();
-        
-    }
 
-    private void SpawnSequenceCompleted()
+    }  
+
+    public void StorePlayer(Player player)
     {
-        renderer.enabled = true;
-
-        spawnIndicator.enabled = false;
-
-        hasSpawned = true;
+        this.player = player;
     }
     private void FollowPlayer()
     {
@@ -71,32 +31,5 @@ public class EnemyMovement : MonoBehaviour
         Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
 
         transform.position = targetPosition;
-    }
-    private void TrayAttack()
-    {
-        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-
-        if(distanceToPlayer <=  playerDetectionRadiusd
-)
-        {
-            PassAway();
-          
-        }
-    }
-    private void PassAway()
-    {
-        // Unparent the particles & play them
-        passAwayParticles.transform.SetParent(null);
-        passAwayParticles.Play();
-
-        Destroy(gameObject);
-    }
-    private void OnDrawGizmos()
-    {
-        if (!gizmos)
-            return;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, playerDetectionRadiusd);
     }
 }
